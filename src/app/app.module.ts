99368c54f223e0f628d3a421592749b557048ca6
ksyunyafimm <ksyunyafimm0498@gmail.com>
2022-04-29 01:44:04 +0300

    Try to add YT video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgxYoutubePlayerModule } from 'ngx-youtube-player';
+import {YoutubeVideoModule} from "./youtube-video/youtube-video.module";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxYoutubePlayerModule.forRoot(),
+    YoutubeVideoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
